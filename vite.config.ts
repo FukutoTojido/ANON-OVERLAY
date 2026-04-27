f4753695e03a4c1ac8d9ff7d05775d6b56a4cf4b
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-04-27 21:40:38 +0700

    feat: config bundler

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [tailwindcss()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].js",
+				assetFileNames: "assets/[name].[ext]",
+			},
+		},
+	},
 });
